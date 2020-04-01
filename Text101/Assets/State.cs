19f1efe175f57ad7f5951e01370dbb1bc98837fb
Cédric Sougné <cedric@sougne.name>
2020-04-01 21:17:46 +0200

    23. Unity Scriptable Objects

---
+++ Text101/Assets/State.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [UnityEngine.CreateAssetMenu(fileName = "anyState", menuName = "State", order = 0)]
+    public class State : UnityEngine.ScriptableObject
+    {
+        [TextArea(14,10)][SerializeField] private string storyText;
+    }
+}
