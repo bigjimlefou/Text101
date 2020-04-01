97ac9425532e31157300b2e70c4810c0ffc9ff5e
Cédric Sougné <cedric@sougne.name>
2020-04-01 21:50:23 +0200

    25. Public Methods & Return Types

--- Text101/Assets/AdventureGame.cs
+++ Text101/Assets/AdventureGame.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Text101Namespace;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
-    [SerializeField] Text textComponent;
+    [SerializeField] private Text textComponent;
+    [SerializeField] private State startingState;
+
+    private State state;
     
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "Hello, here start the game";
+        state = startingState;
+        textComponent.text = startingState.StoryText;
     }
 
     // Update is called once per frame
--- Text101/Assets/State.cs
+++ Text101/Assets/State.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 
-namespace DefaultNamespace
+namespace Text101Namespace
 {
     [UnityEngine.CreateAssetMenu(fileName = "anyState", menuName = "State", order = 0)]
     public class State : UnityEngine.ScriptableObject
     {
         [TextArea(14,10)][SerializeField] private string storyText;
+        public string StoryText => storyText;
     }
 }
